4632ab93f15d6f8643e78af2208db5df2498b29c
Kenzi <a23g32@gmail.com>
2022-04-28 11:19:51 +0800

    change close btn z-index

--- components/ProjectDetailsCard.js
+++ components/ProjectDetailsCard.js
@@ -26,7 +26,7 @@ function ProjectDetailsCard({ id, setSelectedId, projects, is }) {
                     {/* close button */}
                     <div
                         onClick={() => setSelectedId(null)}
-                        className="absolute cursor-pointer right-2 top-2">
+                        className="absolute z-50 cursor-pointer right-2 top-2">
 
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
