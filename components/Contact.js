2ce47c7cce4a1d365caeaac2f908110697490b0b
Kenzi <a23g32@gmail.com>
2022-04-28 11:07:34 +0800

    github btn added

--- components/Contact.js
+++ components/Contact.js
@@ -3,9 +3,10 @@ import IconBtn from './IconBtn';
 
 function Contact() {
     return (
-        <div className="flex flex-row p-3 space-x-5 border-t-2 border-gray-400 md:p-5 ">
+        <div className="flex flex-row justify-center w-full p-3 space-x-5 border-t-2 border-gray-400 md:p-5 ">
             <IconBtn iconName="mdi:gmail" link='mailto:a23g32@gmail.com?' />
             <IconBtn iconName="cib:telegram-plane" link='https://t.me/kenzi0701' />
+            <IconBtn iconName="akar-icons:github-fill" link='https://github.com/programmerkenzi?tab=repositories' />
         </div>
 
     )
